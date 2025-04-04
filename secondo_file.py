2635d048eb0bac31c876aeba804991b1567867e9
__EnderShade__ <decicconunzio@gmail.com>
2025-04-04 11:22:06 +0200

    Update secondo_file.py
    primo esercizio py

--- secondo_file.py
+++ secondo_file.py
@@ -1,1 +1,16 @@
+"""Es 2:  Andare a creare un sistema ripetibile che si occupa di gestire la lunghezza delle stringhe e salvarle, 
+l'utente potrà continuare a inserire dati finché la stringa inserita e più lunga della precedente, 
+alla fine stamperà l'insieme delle stringhe date in input divise da virgole e quante stringhe ha inserito"""
 
+start = bool(input("Vuoi iniziare? (True/False): "))
+test_stringa = input("Inserire la stringa di test: ") # il mio test
+insieme_stringhe = []
+
+while start:# inizia il gioco se la condizione è verificata
+    stringa = input("Inserire una stringa qualsiasi: ")
+    while len(test_stringa) < len(stringa):
+        insieme_stringhe.append(stringa)
+        if len(test_stringa) >= len(stringa):# stampa l'insieme se la condizione è verificata
+            print(insieme_stringhe)
+            break
+        
